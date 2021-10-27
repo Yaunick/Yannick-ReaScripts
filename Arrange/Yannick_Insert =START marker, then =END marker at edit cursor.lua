657f2e94b7e7f623173a4f3f2d4f52f614890874
Yaunick <53025085+Yaunick@users.noreply.github.com>
2021-10-27 15:54:42 +0300

    Updated the Script

--- Arrange/Yannick_Insert =START marker, then =END marker at edit cursor.lua
+++ Arrange/Yannick_Insert =START marker, then =END marker at edit cursor.lua
@@ -1,21 +1,31 @@
 -- @description Yannick_Insert =START marker, then =END marker at edit cursor
 -- @author Yannick
--- @version 1.0
+-- @version 1.1
 -- @about
 --   go to the guide https://github.com/Yaunick/Yannick-ReaScripts-Guide/blob/main/Guide%20to%20using%20my%20scripts.md
 -- @changelog
---   Initial release
+--   + Added protection against incorrect user settings
 -- @contact b.yanushevich@gmail.com
 -- @donation https://www.paypal.com/paypalme/yaunick?locale.x=ru_RU
-  
-  reaper.Undo_BeginBlock()
-  reaper.PreventUIRefresh(1)
   
   ---//////---COLOR---\\\\\---
     R = 186   ----Red
     G = 0     ----Green
     B = 0     ----Blue
   ---\\\\\\\\\\||//////////---
+  
+  function bla() end
+  function nothing() reaper.defer(bla) end
+  
+  if (not tonumber(R) or not tonumber(G) or not tonumber(B))
+  or (R < 0 or G < 0 or B < 0)
+  then
+    reaper.MB('Incorrect values at the beginning of the script', 'Error', 0)
+    nothing() return
+  end
+  
+  reaper.Undo_BeginBlock()
+  reaper.PreventUIRefresh(1)
   
     local cur_pos = reaper.GetCursorPosition()
     local color = reaper.ColorToNative(R,G,B)|0x1000000
