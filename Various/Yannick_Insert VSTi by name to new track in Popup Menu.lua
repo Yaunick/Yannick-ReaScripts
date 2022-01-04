75f78d601fcbbabdaeb757483033460cfb3ffbbd
Yaunick <53025085+Yaunick@users.noreply.github.com>
2022-01-04 19:21:24 +0300

    Updated the script

--- Various/Yannick_Insert VSTi by name to new track in Popup Menu.lua
+++ Various/Yannick_Insert VSTi by name to new track in Popup Menu.lua
@@ -1,10 +1,10 @@
 -- @description Yannick_Insert VSTi by name to new track in Popup Menu
 -- @author Yannick
--- @version 1.1
+-- @version 1.2
 -- @about
 --   go to the guide https://github.com/Yaunick/Yannick-ReaScripts-Guide/blob/main/Guide%20to%20using%20my%20scripts.md
 -- @changelog
---   + Improved creation of a menu with plugins, the names of which contain ">", "<", "#"
+--   + Added new record mode settings
 -- @contact b.yanushevich@gmail.com
 -- @donation https://www.paypal.com/paypalme/yaunick?locale.x=ru_RU
  
@@ -41,7 +41,25 @@
   --Instrument settings--------------------------------------------------------------
     
     defaults_for_new_track = false
-    monitor_rec_for_new_track = true
+    -- if false then --------------------
+      monitor_rec_for_new_track = true
+      
+      record_mode = 1      -- 1 = Record: input (audio or MIDI)
+      
+                           -- 2 = Record: MIDI overdub
+                           -- 3 = Record: MIDI replace
+                           -- 4 = Record: MIDI touch-replace
+                           -- 5 = Record: MIDI latch-replace
+                           
+                           -- 6 = Record: output (MIDI)
+                           
+                           -- 7 = Record: input (force MIDI)
+                           
+                           -- 8 = Record: disable (input monitoring only)
+                             
+    -- end ------------------------------
+  
+    
     
     set_exclusive_rec_arm = 2      -- 0 = no rec arm 
                                    -- 1 = auto rec arm (works in REAPER 6.30 or higher)
@@ -79,14 +97,15 @@
   or (#table_fx == 0 or find_number(table_fx) == true)
   or (defaults_for_new_track ~= true and defaults_for_new_track ~= false)
   or (monitor_rec_for_new_track ~= true and monitor_rec_for_new_track ~= false)
+  or (record_mode ~= 1 and record_mode ~= 2 and record_mode ~= 3 and record_mode ~= 4 
+    and record_mode ~= 5 and record_mode ~= 6 and record_mode ~= 7 and record_mode ~= 8)
   or (set_exclusive_rec_arm ~= 0 and set_exclusive_rec_arm ~= 1 and set_exclusive_rec_arm ~= 2)
   or (not tonumber(R) or not tonumber(G) or not tonumber(B))
   or (R < 0 or B < 0 or B < 0)
   then
     reaper.MB
     (
-    'Incorrect value for "name" or "defaults_for_new_track" ' ..
-    'or "monitor_rec_for_new_track" or "RGB" parameters. Look at the beginning of the script',
+    'Incorrect values at the beginning of the script',
     'Error',
     0
     )
@@ -125,12 +144,6 @@
          reaper.SetTrackColor(track, reaper.ColorToNative(R,G,B)|0x1000000)
        end
      
-       if monitor_rec_for_new_track == true then
-         reaper.SetMediaTrackInfo_Value( track, 'I_RECMON', 1)
-       else
-         reaper.SetMediaTrackInfo_Value( track, 'I_RECMON', 0)
-       end
-     
        if set_exclusive_rec_arm == 2 then
          reaper.Main_OnCommand(40491,0) -- unarm all tracks
          reaper.SetMediaTrackInfo_Value( track, 'I_RECARM', 1)
@@ -139,9 +152,37 @@
          reaper.SetMediaTrackInfo_Value( track, 'I_RECARM', 1)
          reaper.SetMediaTrackInfo_Value( track, 'B_AUTO_RECARM', 1)
        end
-     
+       
        if defaults_for_new_track == false then
+         
+         if monitor_rec_for_new_track == true then
+           reaper.SetMediaTrackInfo_Value( track, 'I_RECMON', 1)
+         else
+           reaper.SetMediaTrackInfo_Value( track, 'I_RECMON', 0)
+         end
+       
          reaper.SetMediaTrackInfo_Value( track, 'I_RECINPUT', 4096+(63<<5))
+         
+         if record_mode == 1 then
+           record_mode = 0
+         elseif record_mode == 2 then
+           record_mode = 7
+         elseif record_mode == 3 then
+           record_mode = 8
+         elseif record_mode == 4 then
+           record_mode = 9
+         elseif record_mode == 5 then
+           record_mode = 16
+         elseif record_mode == 6 then
+           record_mode = 4
+         elseif record_mode == 7 then
+           record_mode = 15
+         elseif record_mode == 8 then
+           record_mode = 2
+         end
+       
+         reaper.SetMediaTrackInfo_Value( track, 'I_RECMODE', record_mode)
+         
        end
        
        reaper.TrackFX_Show(track, reaper.TrackFX_GetCount(track)-1, 3)
