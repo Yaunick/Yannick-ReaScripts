cae780d7ab2a7d999cf358a622b8e8d09116a300
Yaunick <53025085+Yaunick@users.noreply.github.com>
2022-02-16 03:16:13 +0300

    Updated the script

--- Tracks/Yannick_Offline all FX from selected tracks - Clear all saves (slot n).lua
+++ Tracks/Yannick_Offline all FX from selected tracks - Clear all saves (slot n).lua
@@ -1,10 +1,10 @@
 -- @description Yannick_Offline all FX from selected tracks - Clear all saves (slot n)
 -- @author Yannick
--- @version 1.0
+-- @version 1.1
 -- @about
 --   go to the guide https://github.com/Yaunick/Yannick-ReaScripts-Guide/blob/main/Guide%20to%20using%20my%20scripts.md
 -- @changelog
---   Initial release
+--   + Added slot number to warning
 -- @contact b.yanushevich@gmail.com
 -- @donation https://www.paypal.com/paypalme/yaunick?locale.x=ru_RU 
 
@@ -20,6 +20,6 @@
     reaper.MB("Nothing to clean", "Warning", 0)
   else
     reaper.SetProjExtState(0, "Selected_tracks_fx_yannick_reasc" .. slot, "", "")
-    reaper.MB("Success! All offline states of FX have been removed", "Warning", 0)
+    reaper.MB("Success! All offline states of FX from tracks have been removed (slot " .. slot .. ")", "Warning", 0)
   end
   nothing()
