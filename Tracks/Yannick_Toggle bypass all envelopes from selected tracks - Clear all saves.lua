1a18964f92fd25758ec9ea157e6249248cba149b
Yaunick <53025085+Yaunick@users.noreply.github.com>
2022-02-19 22:14:40 +0300

    Updated the script

--- Tracks/Yannick_Toggle bypass all envelopes from selected tracks - Clear all saves.lua
+++ Tracks/Yannick_Toggle bypass all envelopes from selected tracks - Clear all saves.lua
@@ -1,10 +1,10 @@
 -- @description Yannick_Toggle bypass all envelopes from selected tracks - Clear all saves
 -- @author Yannick
--- @version 1.0
+-- @version 1.1
 -- @about
 --   go to the guide https://github.com/Yaunick/Yannick-ReaScripts-Guide/blob/main/Guide%20to%20using%20my%20scripts.md
 -- @changelog
---   Initial release
+--   + fixed error in warning text
 -- @contact b.yanushevich@gmail.com
 -- @donation https://www.paypal.com/paypalme/yaunick?locale.x=ru_RU 
   
@@ -16,6 +16,6 @@
     reaper.MB("Nothing to clean", "Warning", 0)
   else
     reaper.SetProjExtState(0, "Selected_tracks_active_envelopes_yannick_reasc_toggle_bypass_ind", "", "")
-    reaper.MB("Success! All bypass states of FX from tracks have been removed", "Warning", 0)
+    reaper.MB("Success! All bypass states of envelopes from tracks have been removed", "Warning", 0)
   end
   nothing()
