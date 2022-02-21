6e1ab11f42953f1a512a18cf85bde9345e5a51ec
Yaunick <53025085+Yaunick@users.noreply.github.com>
2022-02-21 22:47:16 +0300

    Updated the script

--- Items/Yannick_Unselect every odd item of selected items.lua
+++ Items/Yannick_Unselect every odd item of selected items.lua
@@ -1,10 +1,10 @@
 -- @description Yannick_Unselect every odd item of selected items
 -- @author Yannick
--- @version 1.0
+-- @version 1.1
 -- @about
 --   go to the guide https://github.com/Yaunick/Yannick-ReaScripts-Guide/blob/main/Guide%20to%20using%20my%20scripts.md
 -- @changelog
---   Initial release
+--   + some code improvements
 -- @contact b.yanushevich@gmail.com
 -- @donation https://www.paypal.com/paypalme/yaunick?locale.x=ru_RU
   
@@ -23,16 +23,14 @@
     t[i] = reaper.GetSelectedMediaItem(0,i-1)
   end
   
-  local j = 2
-  while j <= #t+1 do
-    local tr_1 = reaper.GetMediaItemTrack(t[j-1])
-    if not t[j] then
-      tr_2 = reaper.GetMediaItemTrack(t[j-1])
-    else
-      tr_2 = reaper.GetMediaItemTrack(t[j])
+  local j = 1
+  while j <= #t do
+    local tr_1 = reaper.GetMediaItemTrack(t[j])
+    reaper.SetMediaItemSelected(t[j], false)
+    reaper.UpdateItemInProject(t[j])
+    if t[j+1] then
+      tr_2 = reaper.GetMediaItemTrack(t[j+1])
     end
-    reaper.SetMediaItemSelected(t[j-1], false)
-    reaper.UpdateItemInProject(t[j-1])
     if tr_1 == tr_2 then
       j = j + 2
     else  
@@ -42,4 +40,5 @@
   
   reaper.Undo_EndBlock('Unselect every odd item of selected items', -1)
   reaper.PreventUIRefresh(-1)  
+ 
  
